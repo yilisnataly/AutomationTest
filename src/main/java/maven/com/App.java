a8989c0ae7f7932e3930c907774176c8b5d35423
Yilis Ramirez <yilis.nataly@gmail.com>
2019-09-15 23:36:29 +0200

    API automation

---
+++ src/main/java/maven/com/App.java
@@ -0,0 +1,13 @@
+package maven.com;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
